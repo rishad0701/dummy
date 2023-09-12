d17d1d1765c97e2cdd7d0a2a0724fd0954394daa
Rishad Achukatla <rishad.achukatla@acsicorp.com>
2023-09-12 15:01:50 +0530

    add

---
+++ build/precache-manifest.c2ea468bcbad72f8f1d933f3ddeeeaa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d04958ceadfa172e59b2c2cac727aae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901e651147a58b616eb1",
+    "url": "/static/css/main.2044c8a7.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901e651147a58b616eb1",
+    "url": "/static/js/main.028b1d52.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
