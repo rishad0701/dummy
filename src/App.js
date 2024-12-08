febf018f8e0c6ae3a268170c900be4acd74e76bc
Rishad Achukatla <rishad.achukatla@acsicorp.com>
2024-12-08 17:35:06 +0530

    dummy changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
+          <Route path='/services' component={Services} />  // changes in dummy
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
         </Switch>
